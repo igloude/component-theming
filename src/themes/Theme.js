2685ed3e221d6a6f9a8b08826ad7523539376f09
Ian Gloude <igloude@gmail.com>
2020-10-27 11:59:31 -0500

    rework themes

--- src/themes/Theme.js
+++ src/themes/Theme.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Theme = ({ children, theme }) => {
-  return <div className={`theme__${theme}`}>{children}</div>;
+import Button from '../button/Button';
+import Input from '../input/Input';
+
+const Theme = ({ theme }) => {
+  return (
+    <div className={`theme theme__${theme}`}>
+      <div className="inputRow">
+        <Input placeholder="First Name" />
+        <Input placeholder="Last Name" />
+      </div>
+      <Input placeholder="Email" />
+      <div className="actionsRow">
+        <Button text="Clear" theme="secondary" />
+        <Button text="Submit" />
+      </div>
+    </div>
+  );
 };
 
 Theme.propTypes = {
-  children: PropTypes.node.isRequired,
   theme: PropTypes.oneOf(['default', 'dark', 'neon']),
 };
 
