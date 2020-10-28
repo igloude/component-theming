e88947287be99a16995a1e5909b9e94379e58bd4
Ian Gloude <igloude@gmail.com>
2020-10-28 10:25:16 -0500

    more setup

--- local/Deprecated.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Deprecated() {
-  return <p style={{ color: 'red', fontWeight: '700', margin: '0', display: 'inline-block' }}>DEPRECATED</p>;
-}
